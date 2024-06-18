154a0f4910e73bb6d8e3b34c58921f1bce423cf4
eberton.franca <ebertonsf@gmail.com>
2024-06-18 20:24:33 -0300

    :tada: first commit

---
+++ Day_1/bool.py
@@ -0,0 +1,12 @@
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa
+# Só existem duas respostas possíveis
+# Sim (True) ou não (False)
+# Existem vários operadores para "questionar"
+# Questiona se um valor é igual ao outro
+
+
+#Operador lógico == 
+
+print(10 == 10)
+print(10 == 9)
